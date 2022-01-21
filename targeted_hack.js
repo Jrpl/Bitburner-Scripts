9df57699a4351e3d1d62cf73ad129e2dc6b218c7
Riley-H-99 <97280210+Riley-H-99@users.noreply.github.com>
2022-01-21 05:58:10 -0700

    Create targeted_hack.js

---
+++ targeted_hack.js
@@ -0,0 +1,7 @@
+export async function main(ns, threads, time, target) {
+	const threads1 = ns.args[0];
+	const time1 = ns.args[1];
+	const target1 = ns.args[2];
+	await ns.sleep(time1);
+	await ns.hack(target1, {threads: threads1})	
+}
