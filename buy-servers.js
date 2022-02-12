93f6aad62e294a47a7fd7dc5c4616f47249e0941
Justin Pfeifler <jrpl101998@gmail.com>
2022-02-11 19:30:02 -0600

    Add numServers argument, Fix buy and delete conditions
    You can now pass the number of servers you want to purchase as an argument.
    
    Buying now calculates RAM distribution based on the number of servers passed in.
    
    Deleting will no longer delete existing larger servers and only deletes as many servers as needed.

--- buy-servers.js
+++ buy-servers.js
@@ -1,20 +1,79 @@
 /** @param {NS} ns **/
-import { calcBestRam, deletePurchasedServers } from "utils.js";
+function calcBestRam(ns, numServers) {
+	let ramList = [];
 
-export async function main(ns) {
-	const ram = calcBestRam(ns);
-	deletePurchasedServers(ns);
+	let i = 1;
+	while (ramList.length < 20) {
+		let result = Math.pow(2, i);
+		ramList.push(result);
+		i++;
+	}
 
-	while (ns.getPurchasedServers().length < ns.getPurchasedServerLimit()) {
-		if (ns.getServerMoneyAvailable('home') > ns.getPurchasedServerCost(ram)) {
-			let host = 'pserv-' + ns.getPurchasedServers().length;
-			if (ns.getPurchasedServers().includes(host)) {
-				ns.killall(host);
-				ns.deleteServer(host);
-			}
-			ns.purchaseServer(host, ram);
+	const affordableRamList = ramList.filter(ram => (numServers * ns.getPurchasedServerCost(ram)) <= ns.getServerMoneyAvailable('home'));
+
+	const bestRam = ramList[affordableRamList.length - 1];
+	return bestRam;
+}
+
+function deletePurchasedServers(ns, numServers, newRam) {
+	const pservs = ns.getPurchasedServers();
+
+	let pservObjs = pservs.map(server => {
+		return {
+			'name': server,
+			'ram': ns.getServerMaxRam(server)
 		}
+	})
 
-		await ns.sleep(5000);
+	pservObjs.sort((a, b) => {
+		return a.ram - b.ram;
+	});
+
+	let pservNames = [];
+	
+	pservObjs.forEach((server, index) => {
+		if (ns.getServerMaxRam(server.name) >= newRam) {
+			return;
+		} else if (index < numServers) {
+			ns.killall(server.name);
+			ns.deleteServer(server.name);
+			return pservNames.push(server.name);
+		}
+	});
+
+	return pservNames;
+}
+
+export async function main(ns, numServers = ns.args[0]) {
+	const ram = calcBestRam(ns, numServers);
+	const totalServers = ns.getPurchasedServers().length + numServers;
+	const maxServers = (ns.getPurchasedServerLimit() < totalServers) ? ns.getPurchasedServerLimit() : totalServers;
+	
+	if (totalServers > ns.getPurchasedServerLimit()) {
+		if (await ns.prompt('The number of servers requested is not available. Do you want to delete existing servers with the smallest ram to make room?')) {
+			const pservNames = deletePurchasedServers(ns, totalServers - ns.getPurchasedServerLimit(), ram);
+
+			pservNames.forEach(name => {
+				if (ns.getServerMoneyAvailable('home') > ns.getPurchasedServerCost(ram)) {
+					ns.purchaseServer(name, ram);
+				}
+			})
+
+			while (ns.getPurchasedServers().length < maxServers) {
+				if (ns.getServerMoneyAvailable('home') > ns.getPurchasedServerCost(ram)) {
+					let host = 'pserv-' + ns.getPurchasedServers().length;
+					ns.purchaseServer(host, ram);
+				}
+			}
+		} else {
+			return;
+		}
+	} else {
+		while (ns.getPurchasedServers().length < maxServers) {
+			if (ns.getServerMoneyAvailable('home') > ns.getPurchasedServerCost(ram)) {
+				let host = 'pserv-' + ns.getPurchasedServers().length;
+				ns.purchaseServer(host, ram);
+			}
+		}
 	}
 }
